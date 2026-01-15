93b2983e99dc387c2f2dd637d5c5266f4c21cac5
MinaAlmasi <mina@cc.au.dk>
2026-01-15 22:41:37 +0100

    finish 08

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,5 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    const argsArr = [...args];
+    resultsArr = [];
+
+    arr.forEach((element) => {
+        if (!(argsArr.includes(element))) {
+            resultsArr.push(element);
+        }
+    })
+    return resultsArr
 };
+
+// note: more smooth way to use "filter" (which is the "advanced" solution in the js)
 
 // Do not edit below this line
 module.exports = removeFromArray;
