5453a52d56711cb4a3496cfc6bffc393c59c5fc2
MinaAlmasi <mina@cc.au.dk>
2026-01-16 10:24:55 +0100

    my solution worked, but TOP solution much smoother!

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,27 @@
-const sumAll = function() {
+const sumAll = function(num1, num2) {
+    if (num1 < 0 || num2 < 0) {
+        return "ERROR"
+    } else if (!(num1 % 1 === 0) || !(num2 % 1 === 0)) {
+        return "ERROR"
+    } else if (isNaN(num1) || isNaN(num2)) {
+        return "ERROR"
+    } else if ((typeof num1 == "string" || typeof num2 == "string")) {
+        return "ERROR"
+    }
 
+    sumArr = [];
+
+    if (num2 > num1) {
+        for (let i = num1; i < num2 + 1; i++) 
+            sumArr.push(i)
+    } else {
+        for (let i = num2; i < num1 + 1; i++)
+            sumArr.push(i)
+    }
+
+    sum = sumArr.reduce((total, item) => total + item)
+
+    return sum
 };
 
 // Do not edit below this line
